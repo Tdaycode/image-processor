c756c87c3399bcbc79e85d48db01c56ff5d355fe
Omotayo Ganiyu <ganiyuomotayo2000@gmail.com>
2025-04-17 21:25:17 +0100

    Chore: adding starter template

---
+++ src/test/java/org/imageprocessor/imageprocessor/ImageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imageprocessor.imageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
